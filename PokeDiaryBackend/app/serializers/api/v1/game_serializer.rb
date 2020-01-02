648ac2ac8bf3fbdeb9e9646eb8d406caf1bbfae0
github username <github email address>
2020-01-02 14:48:20 -0500

    final stuff

--- PokeDiaryBackend/app/serializers/api/v1/game_serializer.rb
+++ PokeDiaryBackend/app/serializers/api/v1/game_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::GameSerializer
     include FastJsonapi::ObjectSerializer
-    attributes :type_of_game, :trainer_id, :version, :created_at, :updated_at
+    attributes :type_of_game, :trainer_id, :version, :created_at, :updated_at, :pokemons
   end
   
